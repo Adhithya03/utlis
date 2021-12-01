15df5e4cff76ca622dbe076c25c16f0ad14c0f6a
ADHITHYA S <77617084+Adhithya03@users.noreply.github.com>
2021-12-01 15:26:07 +0530

    Adopted match case feature in python 3.10

--- Assistant 4.0/Assistant 4.0.py
+++ Assistant 4.0/Assistant 4.0.py
@@ -1,62 +1,110 @@
-import time as t,webbrowser as web , pyautogui as pg , os
-
-#I created a file called constants.py as the name impliles it contains all the constants
-import constants
-
-
-#Functions
 def join_meet(link):
 
 	def shot_and_ret_pix_color(x,y,channel):
-		return (pg.screenshot().getpixel((x,y))[channel])
-
+		return ()
+	import time as t
+	import constants
+	import pyautogui as pg
+	import webbrowser as web
 	web.open(link)
 	t.sleep(constants.minimum_cooldown)
-	
+
 	while 1:
-		result_value = shot_and_ret_pix_color ( constants.join_now_x , constants.join_now_y , constants.channel_to_check )
+
+		result_value = pg.screenshot().getpixel((constants.join_now_x,constants.join_now_y))[constants.channel_to_check] 
+		
 		if  result_value > constants.threshold :
 			break
-		
-		else :
-			t.sleep(constants.interval)
-
+	t.sleep(0.1)
 	pg.hotkey("ctrl","e")
 	pg.hotkey("ctrl","d")
 	pg.click(constants.join_now_x,constants.join_now_y)
-	t.sleep(constants.interval)
-	#Gmeet spotlight minize button
-	pg.click(constants.my_status_x , constants.my_status_y)
 
-command =  input ( constants.cursor )
-parsed_command = command.split(" ")
+def main():
+	
+	command =  input ("$ ")
 
-if   command == "j":
-	join_meet(constants.gmeet_link)
+	match command:
 
-elif command =="js":
-	web.open(constants.gmeet_link)
+		case "j":		
+			join_meet(constants.gmeet_link)
 
-elif command == "c":
-	web.open(constants.classroom_home)
+		case "ja":
+			join_meet("https://meet.google.com/lookup/LearnTogetherWithAdhithya?authuser=1")
 
-elif command == "mp":
-	os.system("disp /rotate:90")
+		case "t":
 
-elif command == "ml":
-	os.system("disp /rotate:0")
+			import wget,os
+			import glob
+			link = input("Paste the URL :")
+			link = link.split("=")
+			os.chdir(r"D:\My-works\intern\Thumbnails\Inspiration network")
+			wget.download(fr"http://img.youtube.com/vi/{link[1]}/maxresdefault.jpg")
+			list_of_files = glob.glob("*")
+			latest_file = max(list_of_files, key=os.path.getctime)
+			os.startfile(latest_file)
 
-elif parsed_command[0] =="b":
-	os.system(f"disp /brightness:{parsed_command[-1]}")
+		case "x":
+			import os
+			os.startfile(r"D:\My-works\dev\ahk scripts\latex_obsi.ahk")
 
-elif parsed_command[0] =="c":
-	os.system(f"disp /contrast:{parsed_command[-1]}")
+		case "l":
+			import constants
+			from Adafruit_IO import Client, Data
+			adafruit_client = Client(constants.ADAFRUIT_IO_USERNAME , constants.ADAFRUIT_IO_KEY)
+			light_relay = adafruit_client.feeds('relay1').key
+			adafruit_client.send_data(light_relay,1)
 
-elif parsed_command[0] =="bc":
-	os.system(f"disp /brightness:{parsed_command[-1]}")
-	os.system(f"disp /contrast:{parsed_command[-1]}")
-elif command == "matlab":
-	web.open("https://youtu.be/NSSTkkKRabI?t=249")
+		case "ll":
+			import constants
+			from Adafruit_IO import Client, Data
+			adafruit_client = Client(constants.ADAFRUIT_IO_USERNAME , constants.ADAFRUIT_IO_KEY)
+			light_relay = adafruit_client.feeds('relay1').key
+			adafruit_client.send_data(light_relay,0)
 
-else:
-	web.open(constants.google_query_template + command)
+		case "mp2":
+			import os
+			os.system("disp /device 2 /rotate:90")
+		
+		case "mp1":
+			import os
+			os.system("disp /device 1 /rotate:90")
+
+		case "ml1":
+			import os
+			os.system("disp /device 1 /rotate:0")
+		case "ml2":
+			import os
+			os.system("disp /device 2 /rotate:0")
+
+		case "b1":
+			import os
+			level = input("Brightness :")
+			os.system(f"disp /device 1 /brightness:{level}")
+			os.system(f"disp /device 1 /contrast:{level}")
+
+		case "b":
+			import os
+			level = input("Brightness :")
+			os.system(f"disp /device 2 /brightness:{level}")
+			os.system(f"disp /device 2 /contrast:{level}")
+		case "bs":
+			import webbrowser as web
+			nameOfBook = input("Book name :")
+			web.open(f"https://1lib.net/s/{nameOfBook}")
+
+		case _:
+			print("bs - Book search in 1lib.net\n")
+			print("\nmp - monitor portrait")
+			print("b  - monitor 2 brightness")
+			print("b1  - monitor 1 brightness")
+			print("ml - monitor landscape")
+			print("l  - lights on")
+			print("ll - lights off")
+			print("ll - lights off")
+			print("t  - Download thumbnail")
+			print("x  - AutoHotkey script")
+			print("ja - Join adhithya meet\n")
+			main()
+if __name__ == '__main__':
+	main()
